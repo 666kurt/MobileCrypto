faea557d4b8ab07b61894d02964404124bb42f9a
Maxim <incentivedie@gmail.com>
2024-07-09 16:29:45 +0400

    Add CoinRowView + some extensions

---
+++ MobileCrypto/Modules/Home/Views/CoinRowView.swift
@@ -0,0 +1,73 @@
+//
+//  CoinRowView.swift
+//  MobileCrypto
+//
+//  Created by Максим Шишлов on 09.07.2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: Coin
+    let showHoldingColumn: Bool
+    
+    var body: some View {
+        HStack {
+            leftColumn
+            Spacer()
+            if showHoldingColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .foregroundStyle(Color.theme.accent)
+                .font(.headline)
+                .padding(.leading, 6)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(spacing: 0) {
+            VStack(alignment: .trailing) {
+                Text(coin.currentHoldingPrice.asCurrencyWithDecimal())
+                    .bold()
+                Text((coin.currentHoldings ?? 0).asStringNumber())
+            }
+            .foregroundStyle(Color.theme.accent)
+        }
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWithDecimal())
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "0%")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0
+                    ? Color.theme.green
+                    : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+    
+}
+
+#Preview {
+    CoinRowView(coin: DeveloperPreview.shared.coin, showHoldingColumn: true)
+}
---
+++ MobileCrypto/Utilities/Double+ext.swift
@@ -0,0 +1,59 @@
+//
+//  Double+ext.swift
+//  MobileCrypto
+//
+//  Created by Максим Шишлов on 09.07.2024.
+//
+
+import Foundation
+
+extension Double {
+    
+    /// Convent a Double into Currency
+    /// ```
+    /// Convert 1234.56 -> $1.234,65
+    /// Convert 12.3456 -> $12.3465
+    /// Convert 0.12345 -> $0.123456
+    /// ```
+    
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 6
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Convent a Double into Currency as String
+    /// ```
+    /// Convert 1234.56 -> "$1.234,65"
+    /// Convert 12.3456 -> "$12.3465"
+    /// Convert 0.12345 -> "$0.123456"
+    /// ```
+    
+    func asCurrencyWithDecimal() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter.string(from: number) ?? "$0.00"
+    }
+    
+    /// Convent a Double into String representation
+    /// ```
+    /// Convert 12.3456 -> "12.34"
+    /// ```
+    
+    func asStringNumber() -> String {
+        String(format: "%.2f", self)
+    }
+    
+    /// Convent a Double into String representation with percent
+    /// ```
+    /// Convert 12.3456 -> "12.34%"
+    /// ```
+    
+    func asPercentString() -> String {
+        asStringNumber() + "%"
+    }
+    
+    
+}
